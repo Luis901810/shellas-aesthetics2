c33c7967f6a82f8fa97ec9b99f3bf7794e8b289e
luis901810 <luis901810@gmail.com>
2024-07-01 16:24:10 -0500

    abaut

--- src/Views/Landing/Landing.jsx
+++ src/Views/Landing/Landing.jsx
@@ -6,11 +6,9 @@ const Landing = () =>{
         <div>
             
             {/* <h1>Welcome to Shellas Aesthetics!</h1>
-                <p> A One Stop Shop for all your Spa services Needs. A deposit of 30$ is required upon booking to secure appointment slot.</p>
-                <p>I accept cash app via $shellasaesthetics. For payments I accept Cash, Card Payments, & telechecks.</p>
-                <p>Deposits required for all appointments. Thank you for choosing Shellas Aesthetics, we are happy to service you.</p> 
-                <p>No kids, food, and NO disruptive behaviours within spa clinic. We have a no extra guest policy.</p> 
-                <p>Email or text to book appointment, Thank You</p>
+               <p>Welcome to Shellas Aesthetics, Skin care is more than a passion, its also a lifestyle. 
+               Striving to help people achieve their skin goals. Different Facial treatments available to achieve your personal skin goals. 
+               Love the skin your in.</p>
              */}
             
         
