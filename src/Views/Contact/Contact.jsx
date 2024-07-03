eb0876515b620cc84598371a3e69181b446b6352
luis901810 <luis901810@gmail.com>
2024-07-03 16:24:17 -0500

    landing

--- src/Views/Contact/Contact.jsx
+++ src/Views/Contact/Contact.jsx
@@ -1,10 +1,67 @@
-
-
+import { Link } from "react-router-dom";
+import contact from "../../assets/contact.jpg"
+import styles from "./Contact.module.css"
+import { useEffect } from "react";
+import ScrollReveal from 'scrollreveal';
 
 const Contact = () =>{
+
+    const scrollRevealOption={
+        distance: "50px",
+        origin: "bottom",
+        duration: 1000,
+
+    };
+
+    useEffect(() => {
+        ScrollReveal().reveal(`.${styles.subtitle}`,{
+            ...scrollRevealOption,
+        delay: 500
+        });
+       
+        ScrollReveal().reveal(`.${styles.title}`, {
+            ...scrollRevealOption,
+            delay: 1000,
+        });
+        ScrollReveal().reveal(`.${styles.link}`, {
+            ...scrollRevealOption,
+            delay: 1500,
+        });
+        ScrollReveal().reveal(`.${styles.img}`, {
+            ...scrollRevealOption,
+            origin: "left",
+        });
+      
+    
+    }, []);
+
     return(
-        <div>
-            <h2>Soy el contacto</h2>
+        <div className={styles.container}>
+            <div className={styles.img}>
+                <img  src={contact} className={styles.img} alt="conctact"/>
+            </div>
+                <div className={styles.contect}>
+                    <h2 className={styles.title}>Contact Us</h2>
+                    <ul className={styles.link}>
+                    <li>
+                        <Link to="#" className={styles.lista}> 
+                        <i className={`bi bi-envelope-arrow-up me-2 ${styles.email}`}></i>shellasaesthetics@gmail.com 
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to="#" className={styles.lista}> 
+                        <i className={`bi bi-telephone me-2  ${styles.cel}`}></i>+1 774-415-0836
+                        </Link>
+                    </li>
+
+                    </ul>
+
+
+                </div>
+
+            
+            
         </div>
     )
 }
--- src/Views/Landing/Landing.jsx
+++ src/Views/Landing/Landing.jsx
@@ -1,16 +1,48 @@
 import About from"../About/About";
+import Contact from "../Contact/Contact";
+import styles from "./Landing.module.css";
+import ScrollReveal from 'scrollreveal';
+import { useEffect } from "react";
 
 
 const Landing = () =>{
 
+    const scrollRevealOption={
+        distance: "50px",
+        origin: "bottom",
+        duration: 1000,
+
+    };
+    useEffect(() => {
+        ScrollReveal().reveal(`.${styles.title}`,{
+            ...scrollRevealOption,
+        delay: 500
+        });
+       
+        ScrollReveal().reveal(`.${styles.subtitle}`, {
+            ...scrollRevealOption,
+            delay: 1000,
+        });
+        ScrollReveal().reveal(`.${styles.description}`, {
+            ...scrollRevealOption,
+            delay: 1500,
+        });
+       
+       
+    
+    }, []);
+
     return(
-        <div>
+        <div className={styles.container} >
             
-            <h1>Welcome to Shellas Aesthetics!</h1>
-               <p>Welcome to Shellas Aesthetics, Skin care is more than a passion, its also a lifestyle. 
+           
+            <h2 className={styles.title}> Shellas Aesthetics!</h2>
+            <h4 className={styles.subtitle}>Welcome to Shellas Aesthetics</h4>
+               <p className={styles.description}> Skin care is more than a passion, its also a lifestyle. 
                Striving to help people achieve their skin goals. Different Facial treatments available to achieve your personal skin goals. 
                Love the skin your in.</p>
              <About />
+             <Contact />
 
         
         
